8350e6e8ec3e0ecbd27ce4815c3f567c1ec734fd
jemus42 <jemus42@quantenbrot.de>
2016-05-13 22:55:40 +0200

    Switch preset inputs

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -8,15 +8,15 @@ shinyUI(
       fluidPage(
         #### Column 1 ####
         column(4,
-          fluidRow(
-              h2("1. Number of Attributes per Person"),
-              column(6,
-                numericInput("minimum1", "Minimum", value = 4, min = 1, max = 100, step = 1, width = "100%")
-              ),
-              column(6,
-                numericInput("maximum1", "Maximum", value = 8, min = 1, max = 100, step = 1, width = "100%")
-              )
-          ),
+               h3("1. Number of Attributes per Person"),
+               fluidRow(
+                   column(6,
+                     numericInput("minimum1", "Minimum", value = 4, min = 1, max = 100, step = 1, width = "100%")
+                   ),
+                   column(6,
+                     numericInput("maximum1", "Maximum", value = 8, min = 1, max = 100, step = 1, width = "100%")
+                   )
+               ),
               plotOutput("plot1"), checkboxInput("plot1_fixy", "Fix y to [0, 1]", value = F),
           fluidRow(
               column(6,
@@ -27,54 +27,29 @@ shinyUI(
                      numericInput("probability1", "Probability",
                                   value = 0.05, min = 0.001, max = 1, step = 0.01, width = "100%")
               ),
-              h3("Probability Presets"),
+              h4("Probability Presets"),
               fluidRow(
-                column(6,
-                       actionButton("1_uni", "Uniform", width = "100px"),
-                       actionButton("1_norm", "Normal", width = "100px"),
-                       actionButton("1_pois", "Poisson", width = "100px"),
-                       actionButton("1_exp", "Exponential", width = "100px")
-                       ),
-                column(6,
-                       fluidRow(numericInput("1_norm_mean", "Mean", value = 0, width = "80%"),
-                                numericInput("1_norm_sd", "SD", value = 1, width = "80%")
-                                ),
-                       numericInput("1_pois_lambda", "Lamda", value = 6),
-                       numericInput("1_exp_rate", "Rate", value = 0.1)
-                       )
+                column(12, selectInput("preset_types1", "Type of Probability",
+                             choices = c("Uniform", "Normal", "Poisson", "Exponential"),
+                             selected = "Normal", selectize = F, width = "80%")),
+                conditionalPanel("input.preset_types1 == 'Normal'",
+                                 column(6, numericInput("1_norm_mean", "Mean", value = 0, width = "100%")),
+                                 column(6, numericInput("1_norm_sd", "SD", value = 1, width = "100%")
+                                 )),
+                conditionalPanel("input.preset_types1 == 'Poisson'",
+                                 column(6, numericInput("1_pois_lambda", "Lamda", value = 6)
+                                 )),
+                conditionalPanel("input.preset_types1 == 'Exponential'",
+                                 column(6, numericInput("1_exp_rate", "Rate", value = 0.1)
+                                 )),
+                column(12, actionButton("preset_go1", "Apply Preset", width = "100%"))
               )
-              # fluidRow(
-              #   inputPanel(
-              #     column(12,
-              #       actionButton("1_uni", "Uniform", width = "100px")
-              #     )
-              #   ),
-              #   tags$br(),
-              #   inputPanel(
-              #     fluidRow(
-              #     column(6, actionButton("1_norm", "Normal", width = "100px")),
-              #     column(6,
-              #            numericInput("1_norm_mean", "Mean", value = 0),
-              #            numericInput("1_norm_sd", "SD", value = 1))
-              #     )
-              #   ),
-              #   tags$br(),
-              #   inputPanel(
-              #     column(6, tags$br(), actionButton("1_pois", "Poisson", width = "100px")),
-              #     column(6, numericInput("1_pois_lambda", "Lamda", value = 6))
-              #   ),
-              #   tags$br(),
-              #   inputPanel(
-              #     column(6, tags$br(), actionButton("1_exp", "Exponential", width = "100px")),
-              #     column(6, numericInput("1_exp_rate", "Rate", value = 0.1))
-              #   )
-              # )
             )
         ),
         #### Column 2 ####
         column(4,
+               h3("2. Probability of Each Category"),
                fluidRow(
-                 h2("2. Probability of Each Category"),
                  column(6,
                         numericInput("minimum2", "Minimum", value = 4, min = 1, step = 1, width = "100%")
                  ),
@@ -83,7 +58,6 @@ shinyUI(
                  )
                ),
                plotOutput("plot2"), checkboxInput("plot2_fixy", "Fix y to [0, 1]", value = F),
-               fluidRow(
                  column(6,
                         numericInput("category", "Category",
                                      value = 1, min = 1, max = 100, step = 1, width = "100%")
@@ -92,27 +66,36 @@ shinyUI(
                         numericInput("probability2", "Probability",
                                      value = 0.11, min = 0.001, max = 1, step = 0.01, width = "100%")
                  ),
-                 h3("Probability Presets"),
-                 fluidRow(
-                   column(6,
-                          actionButton("2_uni", "Uniform", width = "100px"),
-                          actionButton("2_norm", "Normal", width = "100px"),
-                          actionButton("2_pois", "Poisson", width = "100px"),
-                          actionButton("2_exp", "Exponential", width = "100px")
-                   ),
-                   column(6,
-                          fluidRow(numericInput("2_norm_mean", "Mean", value = 0, width = "80%"),
-                                   numericInput("2_norm_sd", "SD", value = 1, width = "80%")
-                          ),
-                          numericInput("2_pois_lambda", "Lamda", value = 6),
-                          numericInput("2_exp_rate", "Rate", value = 0.1)
-                   )
-                 )
+               h4("Probability Presets"),
+               fluidRow(
+                 column(12, selectInput("preset_types2", "Type of Probability",
+                                        choices = c("Uniform", "Normal", "Poisson", "Exponential"),
+                                        selected = "Normal", selectize = F, width = "80%")),
+                 conditionalPanel("input.preset_types2 == 'Normal'",
+                                  column(6, numericInput("2_norm_mean", "Mean", value = 0, width = "100%")),
+                                  column(6, numericInput("2_norm_sd", "SD", value = 1, width = "100%")
+                                  )),
+                 conditionalPanel("input.preset_types2 == 'Poisson'",
+                                  column(6, numericInput("2_pois_lambda", "Lamda", value = 6)
+                                  )),
+                 conditionalPanel("input.preset_types2 == 'Exponential'",
+                                  column(6, numericInput("2_exp_rate", "Rate", value = 0.1)
+                                  )),
+                 column(12, actionButton("preset_go2", "Apply Preset", width = "100%"))
                )
             ),
         #### Column 3 ####
         column(4,
-               h2("3. Simulate")
+               h3("3. Simulate"),
+               fluidRow(
+                 column(6, numericInput("sample_size", "Sample Size", value = "10")),
+                 column(6, numericInput("run_times", "Run N times", value = "10"))
+               ),
+               fluidRow(
+                 column(6, actionButton("sample_random", "Random Sample", width = "100%")),
+                 column(6, actionButton("run_button", "Run", width = "100%"))
+               ),
+               plotOutput("plot3_1")
                )
       ))
     ),
