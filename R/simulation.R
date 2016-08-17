7f01efd55153af95cf31baeb8f071d7636870c96
jemus42 <jemus42@quantenbrot.de>
2016-08-17 23:35:19 +0200

    Formatting

--- R/simulation.R
+++ R/simulation.R
@@ -31,8 +31,8 @@ sim_one_person <- function(prob, a = 10)
   # apply(d.all, 1, sum)
 
   # simpler code
-  ii <- sample(seq_along(prob), size = a, prob = prob, replace = FALSE)
-  res <- rep(0, length(prob))
+  ii      <- sample(seq_along(prob), size = a, prob = prob, replace = FALSE)
+  res     <- rep(0, length(prob))
   res[ii] <- 1
   res
 }
@@ -49,16 +49,18 @@ sim_one_person <- function(prob, a = 10)
 #' @export
 #' @family Simulations
 #' @examples
-#' sim_n_persons(dexp(1:30, .05), n=2, a=10)
-#' sim_n_persons(dexp(1:30, .05), n=2, a=c(1, 30))
-#' sim_n_persons(dexp(1:30, .05), n=2, a=c(1, 30), ap=c(1,4))
-#' sim_n_persons(dexp(1:30, .05), n=2, a=1:5, ap=c(1,1,2,2,3))
+#' sim_n_persons(dexp(1:30, .05), n = 2, a = 10)
+#' sim_n_persons(dexp(1:30, .05), n = 2, a = c(1, 30))
+#' sim_n_persons(dexp(1:30, .05), n = 2, a = c(1, 30), ap = c(1,4))
+#' sim_n_persons(dexp(1:30, .05), n = 2, a = 1:5, ap = c(1,1,2,2,3))
 #'
 sim_n_persons <- function(prob, n, a=10, ap=rep(1/length(a), length(a)))
 {
-  if (length(a) != length(ap))
+  if (length(a) != length(ap)) {
     stop("the number of attributes must match the attribute probabilites")
-  ap <- ap/sum(ap)  # make sure sum of ap equals one (probability)
+  }
+
+  ap  <- ap/sum(ap)  # make sure sum of ap equals one (probability)
   sim <- replicate(n, sim_one_person(prob, a = sample_new(a, 1, prob = ap)) )
   apply(sim, 1, sum)
 }
@@ -69,18 +71,18 @@ sim_n_persons <- function(prob, n, a=10, ap=rep(1/length(a), length(a)))
 #' @import ggplot2
 #' @family Plotting
 #' @examples
-#' draw_n_person_sample(dexp(1:30, rate=.05), n=100, a=10)
-#' draw_n_person_sample(dexp(1:30, rate=.05), n=100, a=1:5, ap=5:1)
+#' draw_n_person_sample(dexp(1:30, rate = .05), n = 100, a = 10)
+#' draw_n_person_sample(dexp(1:30, rate = .05), n = 100, a = 1:5, ap = 5:1)
 #
 draw_n_person_sample <- function(prob, n, a = 10, ap = rep(1/length(a), length(a)))
 {
   res <- sim_n_persons(prob, n, a, ap)
-  df <- data.frame(no = seq_along(res), cat = res)
-  g <- ggplot(df, aes_string(x = "no", y = "cat")) +
-              geom_line() +
-              geom_point() +
-              scale_x_continuous(name = "Category") +
-              scale_y_continuous(name = "Counts", limits = c(0, max(res)))
+  df  <- data.frame(no = seq_along(res), cat = res)
+  g   <- ggplot(df, aes_string(x = "no", y = "cat")) +
+                geom_line() +
+                geom_point() +
+                scale_x_continuous(name = "Category") +
+                scale_y_continuous(name = "Counts", limits = c(0, max(res)))
   return(g)
 }
 
@@ -96,11 +98,12 @@ draw_n_person_sample <- function(prob, n, a = 10, ap = rep(1/length(a), length(a
 #' @export
 #' @family Simulations
 #' @examples
-#' sim_n_persons_x_times(dexp(1:30, .05), n=2, a=c(1,30), ap=1:2, times=100)
-#' sim_n_persons_x_times(dexp(1:30, .05), n=2, a=c(1,30), ti=200, progress="tk" )
-#'
-sim_n_persons_x_times <- function(prob, n, a, ap=rep(1/length(a),
-                                  length(a)), times=100, progress="text")
+#' \dontrun{
+#' sim_n_persons_x_times(dexp(1:30, .05), n = 2, a = c(1,30), ap = 1:2, times = 100)
+#' sim_n_persons_x_times(dexp(1:30, .05), n = 2, a = c(1,30), times = 200, progress = "tk")
+#' }
+sim_n_persons_x_times <- function(prob, n, a, ap = rep(1/length(a),
+                                  length(a)), times = 100, progress = "text")
 {
   if (!interactive()) {
     progress <- "none"
@@ -155,14 +158,14 @@ expected_frequencies <- function(r)
 #' @export
 #' @family Utilities
 #' @examples
-#' r <- sim_n_persons_x_times(dexp(1:30, rate=.05), n=50, a=5:7, times=100)
-#' prob_categories(r, 4, min.prop=.9)
+#' r <- sim_n_persons_x_times(dexp(1:30, rate = .05), n = 50, a = 5:7, times = 100, progress = "none")
+#' prob_categories(r, 4, min.prop = .9)
 #'
 prob_categories <- function(r, m, min.prop=1)
 {
-  s <- apply(r, 1, function(x, min.prop){    # does the sample render more than
-    (sum(x >= m) / length(x)) >= min.prop    # min.prop categories with >=  m attributes
-  }, min.prop = min.prop)
+  s <- apply(r, 1, function(x, min.prop) {    # does the sample render more than
+         (sum(x >= m) / length(x)) >= min.prop    # min.prop categories with >=  m attributes
+       }, min.prop = min.prop)
   sum(s) / nrow(r)
 }
 
@@ -177,11 +180,11 @@ prob_categories <- function(r, m, min.prop=1)
 #' @family Simulations
 #' @examples
 #' \dontrun{
-#' r <- sim_n_persons_x_times_many_n(dexp(1:30, .05), a=7, times=100)
-#' r <- sim_n_persons_x_times_many_n(dexp(1:30, .05), a=5:7, ap=1:3, times=100, prog="tk")
+#' r <- sim_n_persons_x_times_many_n(dexp(1:30, .05), a = 7, times = 100)
+#' r <- sim_n_persons_x_times_many_n(dexp(1:30, .05), a = 5:7, ap = 1:3, times = 100)
 #' }
 sim_n_persons_x_times_many_n <- function(prob, n = seq(10, 80, by = 10), a = 7,
-                               ap=rep(1/length(a), length(a)), times = 100,
+                               ap = rep(1/length(a), length(a)), times = 100,
                                progress = "text")
 {
   if (!interactive()) {
@@ -207,9 +210,9 @@ sim_n_persons_x_times_many_n <- function(prob, n = seq(10, 80, by = 10), a = 7,
 #' @family Utilities
 #' @examples
 #' prob <-  dexp(1:30, .05)
-#' n <- seq(10, 80, by=20)
-#' r <- sim_n_persons_x_times_many_n(prob, n, a=7, times=100)
-#' dd <- calc_probabilities(r, n, ms=1:5, min.props=c(0.9, .95, 1))
+#' n <- seq(10, 80, by = 20)
+#' r <- sim_n_persons_x_times_many_n(prob, n, a = 7, times = 100)
+#' dd <- calc_probabilities(r, n, ms=1:5, min.props = c(0.9, .95, 1))
 #' head(dd)
 #'
 calc_probabilities <- function(r, n, ms, min.props=c(.9, .95, .99))
@@ -217,9 +220,9 @@ calc_probabilities <- function(r, n, ms, min.props=c(.9, .95, .99))
   res <- NULL
   for (m in ms) {
     for (min.prop in min.props) {
-      probs <- sapply(r, prob_categories, m = m, min = min.prop)
+      probs  <- sapply(r, prob_categories, m = m, min = min.prop)
       res.df <- cbind(n = n, m = m, min.prop = min.prop, prob = probs)
-      res <- append(res, list(res.df))
+      res    <- append(res, list(res.df))
     }
   }
   dd <- as.data.frame(do.call(rbind, res))
@@ -235,8 +238,8 @@ calc_probabilities <- function(r, n, ms, min.props=c(.9, .95, .99))
 #' @examples
 #' ## simulate
 #' prob <-  dexp(1:30, .05)      # probabilities for categories
-#' N <- seq(10, 80, by = 10)       # smaple sizes to simulate
-#' r <- sim_n_persons_x_times_many_n(prob, n = N, a = 7, times = 100)
+#' N <- seq(10, 80, by = 10)     # smaple sizes to simulate
+#' r <- sim_n_persons_x_times_many_n(prob, n = N, a = 7, times = 100, progress = "none")
 #'
 #' # calculate and draw
 #' M <- 1:5                      # minimal number of categories to evaluate
