43a178dacd20e8f11133444b6ac761f544fc2ccb
jemus42 <jemus42@quantenbrot.de>
2016-08-17 19:07:19 +0200

    Add references from README.md to package doc

--- R/gridsampler-package.R
+++ R/gridsampler-package.R
@@ -2,7 +2,11 @@
 #' multiple repertory grids.
 #'
 #'
-#' @references  TODO
+#' @references
+#' \itemize{
+#'  \item Green, B. (2004). Personal construct psychology and content analysis. Personal Construct Theory & Practice, 1(3), 82–91.
+#'  \item Jankowicz, D. (2004). The easy guide to repertory grids. Chichester, England: John Wiley & Sons.
+#'  }
 #' @keywords package repgrid
 #' @name gridsampler-package
 #' @docType package
