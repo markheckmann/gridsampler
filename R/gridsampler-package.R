5ec13c31e9053b90d2bb4ec8c85fe5986a7a8bc4
jemus42 <jemus42@quantenbrot.de>
2016-08-17 19:03:27 +0200

    Tweak docs

--- R/gridsampler-package.R
+++ R/gridsampler-package.R
@@ -1,9 +1,10 @@
 #' \pkg{gridsampler} contains tools to simulate the sampling of
 #' multiple repertory grids.
 #'
+#'
 #' @references  TODO
 #' @keywords package repgrid
-#' @name gridsampler
+#' @name gridsampler-package
 #' @docType package
 NULL
 
