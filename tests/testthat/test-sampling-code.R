061a33941e6fdff2308b204d0c8becf9c8ae2222
Mark Heckmann <mark@Marks-MacBook-Pro.local>
2016-08-21 13:21:35 +0200

    rename file to meet testthat conventions

---
+++ tests/testthat/test-sampling-code.R
@@ -0,0 +1,108 @@
+library(gridsampler)
+library(testthat)
+library(BiasedUrn)
+
+context("Sampling plausibility")
+
+
+test_that("Correct number of attributes is returned", {
+  p <- 10:1
+  s1 <- sim_one_person(p, a=length(p))
+  expect_true(all(s1 == 1))
+})
+
+
+
+# Drawing a fixed number of elements from a finite population without replacement,
+# where a) the elements have unequal probabilities of being drawn, and b) the 
+# drawing of one element affects the probability of the remaining, leads to the 
+# multivariate Wallenius' noncentral hypergeometric distribution (MWNHD). 
+# Here we test if our sampling works correctly and the results converge to 
+# the theoretical distribution.
+
+test_that("Converges to theoretical MWNHD for fixed number of attributes", {
+  
+  # use ten random samples to check if our sampling works correctly
+  set.seed(0)
+  res <- NA
+  for (i in 1:10) {
+    
+    cat("\rrun: ", i)
+    flush.console()
+    
+    N <- sample(1:7, 1)                     # number of attributes
+    n.cat <- sample(8:20, 1)                # number of categories
+    P <- dexp(1L:n.cat, .1)                 # category probabilities
+    f <- sim_n_persons(P, n = 1e6, a = N)   # frequency of each category
+    h <- f / sum(f)                         # relative frequencies
+    mu.sim <- h * N                         # times number of attributes drawn
+    
+    # theoretical
+    M <- rep(1, length(P))                  # initial nuber of balls in urn 
+    mu <- meanMWNCHypergeo(m = M, n=N, odds = P, precision = 0)  # Wallenius' means
+    
+    compare
+    res[i] <- all.equal.numeric(target = mu,
+                                current = mu.sim,
+                                check.attributes = FALSE, use.names = FALSE,
+                                tolerance = 0.005)
+  }
+  
+  expect_true(all(res))
+  
+})
+
+
+# If the number of attributes is not fixed, but is itself a random variable,
+# making the situation more complex. Hence, the resulting distribution is a
+# compound distribution with the number of attributes being a random variable
+# used as a hyper-parameter in the second sampling stage.
+# Here we test for the special case, if our sampling works correctly and the
+# results converge to the theoretical compound distribution.
+
+test_that("Covergence to MWNHD for random number of attributes", {
+
+  # Use ten random samples to check if our sampling works correctly.
+  # Calculating the weighted sum of estimated WNMHD means for each N gives.
+  # stage 2 sampling
+  
+  set.seed(0)
+  res <- NA
+  for (i in 1:10) {
+    
+    cat("\rrun: ", i)
+    flush.console()
+    
+    # simulate
+    NN <- sample(1:10, sample(1:5, 1))  # number of attributes
+    NN <- sort(NN)
+    n.cat <- sample(10:20, 1)            # number of categories
+    
+    P <- dexp(1L:n.cat, .1)             # category weights
+    P <- P / sum(P)                     # category probabilities
+    M <- rep(1, length(P))              # initial number of balls in urn 
+    
+    aw <- runif(length(NN))             # attribute weights
+    ap <- aw / sum(aw)                  # attribute probability distribution
+    f <- sim_n_persons_x_times(P, n = 1e6, a = NN, ap = ap, times = 1, progress = "none")
+    h <- f / sum(f)                     # relative frequencies
+    h <- unlist(h)                      # convert to vector
+    mu.sim <- h * sum(ap * NN)          # relative average number of attributes
+    
+    # theoretical compound distribution
+    MU <- mapply(meanMWNCHypergeo, n=NN, 
+                 MoreArgs = list(m = M, odds = P, precision = 0))
+    MU <- t(MU)
+    mu <- as.vector(ap %*% MU)         # calculate weights means
+    
+    # compare
+    res <- all.equal.numeric(target = mu, 
+                             current = mu.sim,
+                             check.attributes = FALSE, use.names = FALSE,
+                             tolerance = 0.005)
+  }
+  expect_true(all(res))
+  
+})
+
+
