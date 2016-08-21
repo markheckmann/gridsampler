2c56c5463b41df98b8382d5610f7e97730bb887d
Mark Heckmann <mark@Marks-MacBook-Pro.local>
2016-08-21 13:51:28 +0200

    finalize tests #27

--- tests/testthat/test-sampling-code.R
+++ tests/testthat/test-sampling-code.R
@@ -1,14 +1,13 @@
 library(gridsampler)
-library(testthat)
 library(BiasedUrn)
 
 context("Sampling plausibility")
 
 
 test_that("Correct number of attributes is returned", {
-  p <- 10:1
-  s1 <- sim_one_person(p, a=length(p))
-  expect_true(all(s1 == 1))
+  p <- 10:1                               # arbitrary weights for attribute
+  s1 <- sim_one_person(p, a=length(p))    # number of attributes is equal to number of categories
+  expect_true(all(s1 == 1))               # hence, we should get one attribute from each category
 })
 
 
@@ -20,20 +19,20 @@ test_that("Correct number of attributes is returned", {
 # Here we test if our sampling works correctly and the results converge to 
 # the theoretical distribution.
 
-test_that("Converges to theoretical MWNHD for fixed number of attributes", {
+test_that("Converges to theoretical MWNHD means for fixed number of attributes", {
   
   # use ten random samples to check if our sampling works correctly
   set.seed(0)
   res <- NA
   for (i in 1:10) {
     
-    cat("\rrun: ", i)
-    flush.console()
+    # cat("\rrun: ", i)
+    # flush.console()
     
     N <- sample(1:7, 1)                     # number of attributes
     n.cat <- sample(8:20, 1)                # number of categories
     P <- dexp(1L:n.cat, .1)                 # category probabilities
-    f <- sim_n_persons(P, n = 1e6, a = N)   # frequency of each category
+    f <- sim_n_persons(P, n = 1e5, a = N)   # frequency of each category
     h <- f / sum(f)                         # relative frequencies
     mu.sim <- h * N                         # times number of attributes drawn
     
@@ -45,7 +44,7 @@ test_that("Converges to theoretical MWNHD for fixed number of attributes", {
     res[i] <- all.equal.numeric(target = mu,
                                 current = mu.sim,
                                 check.attributes = FALSE, use.names = FALSE,
-                                tolerance = 0.005)
+                                tolerance = 0.01)
   }
   
   expect_true(all(res))
@@ -60,8 +59,10 @@ test_that("Converges to theoretical MWNHD for fixed number of attributes", {
 # Here we test for the special case, if our sampling works correctly and the
 # results converge to the theoretical compound distribution.
 
-test_that("Covergence to MWNHD for random number of attributes", {
+test_that("Covergence to theoretical MWNHD means for random number of attributes", {
 
+  skip_on_cran()
+  
   # Use ten random samples to check if our sampling works correctly.
   # Calculating the weighted sum of estimated WNMHD means for each N gives.
   # stage 2 sampling
@@ -70,8 +71,8 @@ test_that("Covergence to MWNHD for random number of attributes", {
   res <- NA
   for (i in 1:10) {
     
-    cat("\rrun: ", i)
-    flush.console()
+    # cat("\rrun: ", i)
+    # flush.console()
     
     # simulate
     NN <- sample(1:10, sample(1:5, 1))  # number of attributes
@@ -84,7 +85,7 @@ test_that("Covergence to MWNHD for random number of attributes", {
     
     aw <- runif(length(NN))             # attribute weights
     ap <- aw / sum(aw)                  # attribute probability distribution
-    f <- sim_n_persons_x_times(P, n = 1e6, a = NN, ap = ap, times = 1, progress = "none")
+    f <- sim_n_persons_x_times(P, n = 1e5, a = NN, ap = ap, times = 1, progress = "none")
     h <- f / sum(f)                     # relative frequencies
     h <- unlist(h)                      # convert to vector
     mu.sim <- h * sum(ap * NN)          # relative average number of attributes
@@ -99,7 +100,7 @@ test_that("Covergence to MWNHD for random number of attributes", {
     res <- all.equal.numeric(target = mu, 
                              current = mu.sim,
                              check.attributes = FALSE, use.names = FALSE,
-                             tolerance = 0.005)
+                             tolerance = 0.01)
   }
   expect_true(all(res))
   
